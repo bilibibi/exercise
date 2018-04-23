91c654e312ab5f81dacde1c2b998f3e6f4b46f15
fyj <franky@mys4s.cn>
2018-04-23 12:13:46 +0800

    I am sorry

--- java/sort/Merge.java
+++ java/sort/Merge.java
@@ -8,53 +8,53 @@ public class Merge {
   }
 
   public static void mergeArray(int[] a,int first,int mid,int last){
-          int i=first,j=mid+1;
-          int[] temp = new int[last-first+1];
-          int k=0;
+    int i=first,j=mid+1;
+    int[] temp = new int[last-first+1];
+    int k=0;
 
-          while(i<=mid && j<=last) {
-                  if (a[i]<=a[j]) {
-                          temp[k++]=a[i++];
-                  } else {
-                          temp[k++]=a[j++];
-                  }
-          }
+    while(i<=mid && j<=last) {
+      if (a[i]<=a[j]) {
+        temp[k++]=a[i++];
+      } else {
+        temp[k++]=a[j++];
+      }
+    }
 
-          if (i<=mid) {
-                  temp[k++]=a[i++];
-          }
+    if (i<=mid) {
+      temp[k++]=a[i++];
+    }
 
-          if(j<=last) {
-                  temp[k++]=a[j++];
-          }
+    if(j<=last) {
+      temp[k++]=a[j++];
+    }
 
-          for (int alen=0; alen<temp.length; alen++) {
-                  a[first+alen] = temp[alen];
-                  System.out.print(temp[alen]+" ");
-          }
-          System.out.println();
+    for (int alen=0; alen<temp.length; alen++) {
+      a[first+alen] = temp[alen];
+      System.out.print(temp[alen]+" ");
+    }
+    System.out.println();
   }
 
   public static void mergeSort(int[] a,int first,int last){
-          if (first < last) {
-                  int mid = (first+last)/2;
-                  mergeSort(a,first,mid);
-                  mergeSort(a,mid+1,last);
-                  mergeArray(a,first,mid,last);
-          }
+    if (first < last) {
+      int mid = (first+last)/2;
+      mergeSort(a,first,mid);
+      mergeSort(a,mid+1,last);
+      mergeArray(a,first,mid,last);
+    }
   }
 
   public static void main(String[] args) {
-          // int[] array =new int[9];
-          // for(int i=0;i<array.length;i++){
-          //   array[i] = (int)(Math.random()*9);
-          //   System.out.print(array[i]+" ");
-          // }
-          // System.out.println();
-          int[] array = new int[] {1,3,5,7,9,2,4,6,8};
-          mergeSort(array,0,array.length-1);
-          for (int i=0; i<array.length; i++) {
-                  System.out.print(array[i]+" ");
-          }
+    // int[] array =new int[9];
+    // for(int i=0;i<array.length;i++){
+    //   array[i] = (int)(Math.random()*9);
+    //   System.out.print(array[i]+" ");
+    // }
+    // System.out.println();
+    int[] array = new int[] {1,3,5,7,9,2,4,6,8};
+    mergeSort(array,0,array.length-1);
+    for (int i=0; i<array.length; i++) {
+      System.out.print(array[i]+" ");
+    }
   }
 }
--- java/sort/Quick.java
+++ java/sort/Quick.java
@@ -1,15 +1,20 @@
+/**
+* 快速排序
+* 平均时间复杂度 O(nlogn)
+*/
 public class Quick {
 
 	private static int[] a = new int[]{5,7,8,9,6,2,4,3,1,5,6,4,3,2,1,7,8,9};
-	
+
 	public Quick(){
-		
+
 	}
-	
+
 	private void quicksort(int left,int right){
 		if(left>=right)
 			return;
 		System.out.println("left="+left+" right="+right);
+
 		int temp=a[left],i=left,j=right,t;
 		while (i!=j) {
 			while (a[j]>=temp && i<j) {
@@ -24,16 +29,17 @@ public class Quick {
 				a[j] = t;
 			}
 		}
+		//交换最左和指针最终停留位置的值
 		if(left!=i){
 			a[left]=a[i];
 			a[i]=temp;
 		}
-		
+
 		for(int s : a){
 			System.out.print(s+" ");
 		}
-		System.out.println("i="+i);
-		
+		System.out.println();
+
 		this.quicksort(left, i-1);
 		this.quicksort(i+1, right);
 	}
@@ -41,7 +47,7 @@ public class Quick {
 	public static void main(String[] args) {
 		Quick quick = new Quick();
 		quick.quicksort(0, a.length-1);
-		
+
 		System.out.println("result:");
 		for(int s : a){
 			System.out.print(s+" ");
