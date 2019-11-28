029ddaf381926e6d0841c04084085eb95400a468
franky <fangyj1126@foxmail.com>
2019-11-29 00:27:36 +0800

    Update .gitignore

--- golang/src/exercise/sort.go
+++ golang/src/exercise/sort.go
@@ -21,6 +21,79 @@ func bubbleSort(sli []int) []int {
 	return sli
 }
 
+// 快速排序（第一种写法）
+func quickSortA(values []int, left, right int) {
+	if len(values) <= 1 {
+		return
+	}
+	temp := values[left]
+	p := left
+	i, j := left, right
+
+	for i <= j {
+		for j >= p && values[j] >= temp {
+			j--
+		}
+		if j >= p {
+			values[p] = values[j]
+			p = j
+		}
+
+		if i <= p && values[i] <= temp {
+			i++
+		}
+		if i <= p {
+			values[p] = values[i]
+			p = i
+		}
+	}
+
+	values[p] = temp
+	//fmt.Println(values)
+
+	if p-left > 1 {
+		//fmt.Println("sort left")
+		quickSortA(values, left, p-1)
+	}
+	if right-p > 1 {
+		//fmt.Println("sort right")
+		quickSortA(values, p+1, right)
+	}
+}
+
+// 快速排序（第二种写法）
+func quickSortB(values []int) {
+	if len(values) <= 1 {
+		return
+	}
+	mid, i := values[0], 1
+	head, tail := 0, len(values)-1
+	for head < tail {
+		//fmt.Println(values)
+		if values[i] > mid {
+			values[i], values[tail] = values[tail], values[i]
+			tail--
+		} else {
+			values[i], values[head] = values[head], values[i]
+			head++
+			i++
+		}
+	}
+	values[head] = mid
+	quickSortB(values[:head])
+	quickSortB(values[head+1:])
+}
+
 func main() {
 	fmt.Println(bubbleSort(sli))
+
+	a := []int{1, 9, 2, 4, 3, 5, 7, 8, 6, 0}
+	fmt.Println("a is ", a)
+	quickSortA(a, 0, len(a)-1)
+	fmt.Println("a is ", a)
+
+	b := []int{1, 9, 2, 4, 3, 5, 7, 8, 6, 0}
+	fmt.Println("b is ", b)
+	quickSortB(b)
+	fmt.Println("b is ", b)
 }
