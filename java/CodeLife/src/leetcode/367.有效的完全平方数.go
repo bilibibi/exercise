7186e92608ed36343ce3743de5eaf42237648547
franky <fangyj1126@foxmail.com>
2020-02-05 16:14:08 +0800

    I would rather be playing SC2.

--- java/CodeLife/src/leetcode/367.有效的完全平方数.go
+++ java/CodeLife/src/leetcode/367.有效的完全平方数.go
@@ -1,3 +1,7 @@
+package main
+
+import "fmt"
+
 /*
  * @lc app=leetcode.cn id=367 lang=golang
  *
@@ -18,13 +22,30 @@ func isPerfectSquare(num int) bool {
 	// return i*i==num
 
 	// 数学定理 (1 + 3 + 5 + ... + (2n - 1) = n ^ 2)
-	i := 1
-	for num > 0 {
-		num -= i
-		i += 2
-	}
-	return num == 0
+	// i := 1
+	// for num > 0 {
+	// 	num -= i
+	// 	i += 2
+	// }
+	// return num == 0
 
+	// 二分查找
+	left, right := 1, num
+	for left < right {
+		mid := (left + right) / 2
+		if mid*mid == num {
+			return true
+		} else if mid*mid < num {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return left*left == num
 }
+
 // @lc code=end
 
+func main() {
+	fmt.Println(isPerfectSquare(16))
+}
